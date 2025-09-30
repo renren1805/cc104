150c0ca2d506b2c6670b9be7a61487a5a423ba34
renren1805 <monteskaren1205@gmail.com>
2025-09-30 15:00:31 +0800

    updating the other variable

--- m23arrayops.py
+++ m23arrayops.py
@@ -1,7 +1,9 @@
 arr_kdm = [10, 20, 30, 40, 50]
-print("First element:", arr[0])
-print("Last element:", arr[-1])
-for x in arr:
+print("First element:", arr_kdm[0])
+print("Last element:", arr_kdm[-1])
+for x in arr_kdm:
     print(x)
 arr_kdm.insert(0, 5)
-print("After insertion:", arr)
+print("After insertion:", arr_kdm)
+arr_kdm.remove(30)
+print("After deletion:", arr_kdm)
